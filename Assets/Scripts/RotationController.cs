be6916299102ed9fbd3ff1830c4b9f04d78f3757
Nick Maltbie <nick.dmalt@gmail.com>
2020-06-07 23:01:56 -0400

    Added patch to bound camera movement

--- Assets/Scripts/RotationController.cs
+++ Assets/Scripts/RotationController.cs
@@ -7,24 +7,33 @@ public class RotationController : MonoBehaviour
     /// <summary>
     /// Sensitivity value for movement.
     /// </summary>
-    public float sensitivity = 10.0f;
+    public float sensitivity = 100.0f;
+    /// <summary>
+    /// Minimum angle of pitch (up and down).
+    /// </summary>
     public float minVerticalAngle = -80f;
-    public float maxVerticalAngle = 90f;
+    /// <summary>
+    /// Maximum angle of pitch (up and down).
+    /// </summary>
+    public float maxVerticalAngle = 80f;
 
     /// <summary>
-    /// Object to change pitch (up and down) when moving mouse
+    /// Object to change pitch (up and down) when moving mouse.
     /// </summary>
     public Transform pitchTarget;
     /// <summary>
-    /// object ot change yaw (left and right) when moving mouse
+    /// object ot change yaw (left and right) when moving mouse.
     /// </summary>
     public Transform yawTarget;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    /// <summary>
+    /// Current pitch attitude
+    /// </summary>
+    private float pitch;
+    /// <summary>
+    /// Current yaw attitude
+    /// </summary>
+    private float yaw;
 
     // Update is called once per frame
     void Update()
@@ -34,11 +43,14 @@ public class RotationController : MonoBehaviour
         float yawRotation = Input.GetAxis("Mouse X");
 
         // Change player input into pitch and yaw units
-        Vector3 pitchChange =  new Vector3(pitchRotation * -1, 0, 0) * Time.deltaTime * this.sensitivity;
-        Vector3 yawChange =  new Vector3(0, yawRotation, 0) * Time.deltaTime * this.sensitivity;
+        pitch +=  pitchRotation * -1 * Time.deltaTime * this.sensitivity;
+        yaw +=  yawRotation * Time.deltaTime * this.sensitivity;
+
+        // Bound pitch values
+        pitch = Mathf.Clamp(pitch, this.minVerticalAngle, this.maxVerticalAngle);
 
         // Rotate target object by modified vector
-        this.pitchTarget.eulerAngles = this.pitchTarget.eulerAngles + pitchChange;
-        this.yawTarget.eulerAngles = this.yawTarget.eulerAngles + yawChange;
+        this.pitchTarget.localRotation = Quaternion.AngleAxis(pitch, Vector3.right);
+        this.yawTarget.localRotation = Quaternion.AngleAxis(yaw, Vector3.up);
     }
 }
