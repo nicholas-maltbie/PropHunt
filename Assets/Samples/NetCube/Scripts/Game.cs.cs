7bcc983303834b019fb457757b5cf5c34a035743
Nick Maltbie <nick.dmalt@gmail.com>
2020-06-11 00:20:45 -0400

    Updated CubeInput variables

--- Assets/Samples/NetCube/Scripts/Game.cs.cs
+++ Assets/Samples/NetCube/Scripts/Game.cs.cs
@@ -24,7 +24,7 @@ public class Game : ComponentSystem
     {
         // Destroy singleton to prevent system from running again
         EntityManager.DestroyEntity(GetSingletonEntity<InitGameComponent>());
-        foreach (var world in World.AllWorlds)
+        foreach (var world in World.All)
         {
             var network = world.GetExistingSystem<NetworkStreamReceiveSystem>();
             if (world.GetExistingSystem<ClientSimulationSystemGroup>() != null)
