0242d1cde49f5b1db9e23ea17c2740ba89a85245
Nick Maltbie <nick.dmalt@gmail.com>
2020-09-01 10:52:06 -0400

    Switched to use builds folder

--- Assets/Editor/ProdBuilder.cs
+++ Assets/Editor/ProdBuilder.cs
@@ -18,42 +18,42 @@ namespace EditorNamespace
 
         public static void Client_Windows()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Windows-Client/Windows-Client.exe", BuildTarget.StandaloneWindows, BuildOptionsClient());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Windows-Client/Windows-Client.exe", BuildTarget.StandaloneWindows, BuildOptionsClient());
         }
 
         public static void Server_Windows()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Windows-Server/Windows-Server.exe", BuildTarget.StandaloneWindows, BuildOptionsServer());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Windows-Server/Windows-Server.exe", BuildTarget.StandaloneWindows, BuildOptionsServer());
         }
 
         public static void Client_Windows64()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Windows64-Client/Windows64-Client.exe", BuildTarget.StandaloneWindows64, BuildOptionsClient());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Windows64-Client/Windows64-Client.exe", BuildTarget.StandaloneWindows64, BuildOptionsClient());
         }
 
         public static void Server_Windows64()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Windows64-Server/Windows64-Server.exe", BuildTarget.StandaloneWindows64, BuildOptionsServer());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Windows64-Server/Windows64-Server.exe", BuildTarget.StandaloneWindows64, BuildOptionsServer());
         }
 
         public static void Client_Linux64()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Linux64-Client/Linux64-Client.exe", BuildTarget.StandaloneLinux64, BuildOptionsClient());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Linux64-Client/Linux64-Client.exe", BuildTarget.StandaloneLinux64, BuildOptionsClient());
         }
 
         public static void Server_Linux64()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "Linux64-Server/Linux64-Server.exe", BuildTarget.StandaloneLinux64, BuildOptionsServer());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/Linux64-Server/Linux64-Server.exe", BuildTarget.StandaloneLinux64, BuildOptionsServer());
         }
 
         public static void Client_OSX()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "OSX-Client/OSX-Client.exe", BuildTarget.StandaloneOSX, BuildOptionsClient());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/OSX-Client/OSX-Client.exe", BuildTarget.StandaloneOSX, BuildOptionsClient());
         }
 
         public static void Server_OSX()
         {
-            BuildPipeline.BuildPlayer(ProdScenes, "OSX-Server/OSX-Server.exe", BuildTarget.StandaloneOSX, BuildOptionsServer());
+            BuildPipeline.BuildPlayer(ProdScenes, "Builds/OSX-Server/OSX-Server.exe", BuildTarget.StandaloneOSX, BuildOptionsServer());
         }
     }
 }
