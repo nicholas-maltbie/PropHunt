f8e9a8ff36d231792de9850d17c80be8e2fc0070
Nick Maltbie <nick.dmalt@gmail.com>
2020-09-01 02:51:44 -0400

    Added development and production building scripts

---
+++ Assets/Editor/DevelopmentBuilder.cs
@@ -0,0 +1,49 @@
+using UnityEditor;
+
+namespace EditorNamespace
+{
+    public class DevelopmentBuilder
+    {
+        private static BuildOptions BuildOptionsClient()
+        {
+            return BuildOptions.Development;
+        }
+
+        private static BuildOptions BuildOptionsServer()
+        {
+            return BuildOptions.Development | BuildOptions.EnableHeadlessMode;
+        }
+
+        private static readonly string[] DevelopmentScenes = {"Assets/Scenes/SampleScene.unity"};
+
+        public static void Client_Windows()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Windows-Client.exe", BuildTarget.StandaloneWindows, BuildOptionsClient());
+        }
+
+        public static void Server_Windows()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Windows-Server.exe", BuildTarget.StandaloneWindows, BuildOptionsServer());
+        }
+
+        public static void Client_Windows64()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Windows64-Client.exe", BuildTarget.StandaloneWindows64, BuildOptionsClient());
+        }
+
+        public static void Server_Windows64()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Windows64-Server.exe", BuildTarget.StandaloneWindows64, BuildOptionsServer());
+        }
+
+        public static void Client_Linux64()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Linux64-Client.exe", BuildTarget.StandaloneLinux64, BuildOptionsClient());
+        }
+
+        public static void Server_Linux64()
+        {
+            BuildPipeline.BuildPlayer(DevelopmentScenes, "Linux64-Server.exe", BuildTarget.StandaloneLinux64, BuildOptionsServer());
+        }
+    }
+}
