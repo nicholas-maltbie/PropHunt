88d594c91af09e6dd197c4b2e14066d4d0946580
Nick Maltbie <nick.dmalt@gmail.com>
2020-09-01 04:07:22 -0400

    Corrected pdoruction build commands

--- Assets/Editor/ProdBuilder.cs
+++ Assets/Editor/ProdBuilder.cs
@@ -16,22 +16,22 @@ namespace EditorNamespace
 
         private static readonly string[] ProdScenes = {"Assets/Scenes/SampleScene.unity"};
 
-        public static void Client_StandaloneWindows()
+        public static void Client_Windows()
         {
             BuildPipeline.BuildPlayer(ProdScenes, "Windows-Client/Windows-Client.exe", BuildTarget.StandaloneWindows, BuildOptionsClient());
         }
 
-        public static void Server_StandaloneWindows()
+        public static void Server_Windows()
         {
             BuildPipeline.BuildPlayer(ProdScenes, "Windows-Server/Windows-Server.exe", BuildTarget.StandaloneWindows, BuildOptionsServer());
         }
 
-        public static void Client_StandaloneWindows64()
+        public static void Client_Windows64()
         {
             BuildPipeline.BuildPlayer(ProdScenes, "Windows64-Client/Windows64-Client.exe", BuildTarget.StandaloneWindows64, BuildOptionsClient());
         }
 
-        public static void Server_StandaloneWindows64()
+        public static void Server_Windows64()
         {
             BuildPipeline.BuildPlayer(ProdScenes, "Windows64-Server/Windows64-Server.exe", BuildTarget.StandaloneWindows64, BuildOptionsServer());
         }
@@ -49,6 +49,11 @@ namespace EditorNamespace
         public static void Client_OSX()
         {
             BuildPipeline.BuildPlayer(ProdScenes, "OSX-Client/OSX-Client.exe", BuildTarget.StandaloneOSX, BuildOptionsClient());
+        }
+
+        public static void Server_OSX()
+        {
+            BuildPipeline.BuildPlayer(ProdScenes, "OSX-Server/OSX-Server.exe", BuildTarget.StandaloneLinux64, BuildOptionsServer());
         }
     }
 }
