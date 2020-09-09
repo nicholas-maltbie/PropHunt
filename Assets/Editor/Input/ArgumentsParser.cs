75b5cecec91d4f81f65b407e381df668c58cb16c
Nick Maltbie <nick.dmalt@gmail.com>
2020-09-09 19:48:39 -0400

    Cleaned up existing files

--- Assets/Editor/Input/ArgumentsParser.cs
+++ Assets/Editor/Input/ArgumentsParser.cs
@@ -27,76 +27,83 @@ using UnityEditor;
 
 namespace UnityBuilderAction.Input
 {
-  public class ArgumentsParser
-  {
-    static string EOL = Environment.NewLine;
-    static readonly string[] Secrets = { "androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass" };
-
-    public static Dictionary<string, string> GetValidatedOptions()
+    public class ArgumentsParser
     {
-      ParseCommandLineArguments(out var validatedOptions);
+        static string EOL = Environment.NewLine;
+        static readonly string[] Secrets = { "androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass" };
 
-      if (!validatedOptions.TryGetValue("projectPath", out var projectPath)) {
-        Console.WriteLine("Missing argument -projectPath");
-        EditorApplication.Exit(110);
-      }
+        public static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out var validatedOptions);
 
-      if (!validatedOptions.TryGetValue("buildTarget", out var buildTarget)) {
-        Console.WriteLine("Missing argument -buildTarget");
-        EditorApplication.Exit(120);
-      }
+            if (!validatedOptions.TryGetValue("projectPath", out var projectPath))
+            {
+                Console.WriteLine("Missing argument -projectPath");
+                EditorApplication.Exit(110);
+            }
 
-      if (!Enum.IsDefined(typeof(BuildTarget), buildTarget)) {
-        EditorApplication.Exit(121);
-      }
+            if (!validatedOptions.TryGetValue("buildTarget", out var buildTarget))
+            {
+                Console.WriteLine("Missing argument -buildTarget");
+                EditorApplication.Exit(120);
+            }
 
-      if (!validatedOptions.TryGetValue("customBuildPath", out var customBuildPath)) {
-        Console.WriteLine("Missing argument -customBuildPath");
-        EditorApplication.Exit(130);
-      }
+            if (!Enum.IsDefined(typeof(BuildTarget), buildTarget))
+            {
+                EditorApplication.Exit(121);
+            }
 
-      const string defaultCustomBuildName = "TestBuild";
-      if (!validatedOptions.TryGetValue("customBuildName", out var customBuildName)) {
-        Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-        validatedOptions.Add("customBuildName", defaultCustomBuildName);
-      } else if (customBuildName == "") {
-        Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-        validatedOptions.Add("customBuildName", defaultCustomBuildName);
-      }
+            if (!validatedOptions.TryGetValue("customBuildPath", out var customBuildPath)) {
+                Console.WriteLine("Missing argument -customBuildPath");
+                EditorApplication.Exit(130);
+            }
 
-      return validatedOptions;
+            const string defaultCustomBuildName = "TestBuild";
+            if (!validatedOptions.TryGetValue("customBuildName", out var customBuildName))
+            {
+                Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+            else if (customBuildName == "")
+            {
+                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+
+            return validatedOptions;
+        }
+
+        static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{EOL}" +
+                $"###########################{EOL}" +
+                $"#    Parsing settings     #{EOL}" +
+                $"###########################{EOL}" +
+                $"{EOL}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
+        }
     }
-
-    static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
-    {
-      providedArguments = new Dictionary<string, string>();
-      string[] args = Environment.GetCommandLineArgs();
-
-      Console.WriteLine(
-        $"{EOL}" +
-        $"###########################{EOL}" +
-        $"#    Parsing settings     #{EOL}" +
-        $"###########################{EOL}" +
-        $"{EOL}"
-      );
-
-      // Extract flags with optional values
-      for (int current = 0, next = 1; current < args.Length; current++, next++) {
-        // Parse flag
-        bool isFlag = args[current].StartsWith("-");
-        if (!isFlag) continue;
-        string flag = args[current].TrimStart('-');
-
-        // Parse optional value
-        bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
-        string value = flagHasValue ? args[next].TrimStart('-') : "";
-        bool secret = Secrets.Contains(flag);
-        string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
-
-        // Assign
-        Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
-        providedArguments.Add(flag, value);
-      }
-    }
-  }
 }
