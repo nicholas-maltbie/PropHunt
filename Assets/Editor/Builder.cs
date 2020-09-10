432342d73314dcebd5d5dddb732dc6920727388e
Nicholas Maltbie <nick.dmalt@gmail.com>
2020-09-09 20:25:47 -0400

    Production Build Patch (#72)
    * Attempt to fix prod build
    
    * Updated with custom build method
    
    * Updated build sequence
    
    * removed debug command
    
    * filtered to just windows 64 build
    
    * changed test utils to only run in editor
    
    * Improved build to exclude the dll for test utils
    
    * Cleaned up existing files
    
    * added newline to builder file

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,75 @@
+// MIT License
+//
+// Copyright (c) 2019-present Webber Takken <webber@takken.io>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Linq;
+using UnityBuilderAction.Input;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace EditorNamespace
+{
+    static class Builder
+    {
+        public static void BuildProject()
+        {
+            // Gather values from args
+            var options = ArgumentsParser.GetValidatedOptions();
+
+            // Gather values from project
+            var scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+
+            BuildOptions selectedOptions = BuildOptions.None;
+            if (options.ContainsKey("buildType") && options["buildType"].Equals("server", StringComparison.OrdinalIgnoreCase))
+            {
+                selectedOptions |= BuildOptions.EnableHeadlessMode;
+                Console.WriteLine("Creating Server Build");
+            }
+            else
+            {
+                Console.WriteLine("Creating Client Build");
+            }
+            if (options.ContainsKey("development"))
+            {
+                EditorUserBuildSettings.development = true;
+                EditorUserBuildSettings.allowDebugging = true;
+                EditorUserBuildSettings.connectProfiler = true;
+
+                selectedOptions |= BuildOptions.Development |
+                    BuildOptions.AllowDebugging |
+                    BuildOptions.ConnectWithProfiler |
+                    BuildOptions.EnableHeadlessMode;
+            }
+
+            // Define BuildPlayer Options
+            var buildOptions = new BuildPlayerOptions {
+                scenes = scenes,
+                locationPathName = options["customBuildPath"],
+                target = (BuildTarget) Enum.Parse(typeof(BuildTarget), options["buildTarget"]),
+                options = selectedOptions,
+            };
+
+            // Perform build
+            BuildReport buildReport = BuildPipeline.BuildPlayer(buildOptions);
+        }
+    }
+}
---
+++ Assets/Editor/Input/ArgumentsParser.cs
@@ -0,0 +1,109 @@
+﻿// MIT License
+//
+// Copyright (c) 2019-present Webber Takken <webber@takken.io>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace UnityBuilderAction.Input
+{
+    public class ArgumentsParser
+    {
+        static string EOL = Environment.NewLine;
+        static readonly string[] Secrets = { "androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass" };
+
+        public static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out var validatedOptions);
+
+            if (!validatedOptions.TryGetValue("projectPath", out var projectPath))
+            {
+                Console.WriteLine("Missing argument -projectPath");
+                EditorApplication.Exit(110);
+            }
+
+            if (!validatedOptions.TryGetValue("buildTarget", out var buildTarget))
+            {
+                Console.WriteLine("Missing argument -buildTarget");
+                EditorApplication.Exit(120);
+            }
+
+            if (!Enum.IsDefined(typeof(BuildTarget), buildTarget))
+            {
+                EditorApplication.Exit(121);
+            }
+
+            if (!validatedOptions.TryGetValue("customBuildPath", out var customBuildPath)) {
+                Console.WriteLine("Missing argument -customBuildPath");
+                EditorApplication.Exit(130);
+            }
+
+            const string defaultCustomBuildName = "TestBuild";
+            if (!validatedOptions.TryGetValue("customBuildName", out var customBuildName))
+            {
+                Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+            else if (customBuildName == "")
+            {
+                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+
+            return validatedOptions;
+        }
+
+        static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{EOL}" +
+                $"###########################{EOL}" +
+                $"#    Parsing settings     #{EOL}" +
+                $"###########################{EOL}" +
+                $"{EOL}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
+        }
+    }
+}
