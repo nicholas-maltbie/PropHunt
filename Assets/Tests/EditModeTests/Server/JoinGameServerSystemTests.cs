71d0ebf3889f652a7d6844a861633fb744c3994d
Nick Maltbie <nick.dmalt@gmail.com>
2020-11-05 18:33:26 -0500

    Created tests for the HandleDisconnectSystem

---
+++ Assets/Tests/EditModeTests/Server/JoinGameServerSystemTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PropHunt.Mixed.Components;
+using PropHunt.Server.Systems;
+using Unity.Entities;
+using Unity.Entities.Tests;
+using Unity.NetCode;
+
+namespace PropHunt.EditMode.Tests.Server
+{
+    /// <summary>
+    /// Tests to verify functionality of the ConnectionSystem
+    /// </summary>
+    [TestFixture]
+    public class JoinGameServerSystemTests : ECSTestsFixture
+    {
+        /// <summary>
+        /// JoinGameServerSystem for testing the system
+        /// </summary>
+        private JoinGameServerSystem joinGameServerSystem;
+
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            this.joinGameServerSystem = base.World.CreateSystem<JoinGameServerSystem>();
+        }
+    }
+}
