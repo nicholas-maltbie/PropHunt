980422f784325dc6f6219419ee841b1f15235f85
Nick Maltbie <nick.dmalt@gmail.com>
2020-11-09 20:38:16 -0500

    Added assert statements ot KCCSystemTests

--- Assets/Tests/EditModeTests/Mixed/KCCSystemTests.cs
+++ Assets/Tests/EditModeTests/Mixed/KCCSystemTests.cs
@@ -55,8 +55,8 @@ namespace PropHunt.EditMode.Tests.Mixed
         public Entity CreateTestPlayer(float3 position, float radius)
         {
             Entity player = PhysicsTestUtils.CreateSphere(base.m_Manager, radius, position, quaternion.Euler(float3.zero), false);
-            base.m_Manager.AddComponent<KCCGrounded>(player);
-            base.m_Manager.AddComponent<KCCGravity>(player);
+            base.m_Manager.AddComponentData<KCCGrounded>(player, new KCCGrounded { groundCheckDistance = 0.5f });
+            base.m_Manager.AddComponentData<KCCGravity>(player, new KCCGravity { gravityAcceleration = new float3(0, -9.8f, 0) });
 
             return player;
         }
@@ -67,17 +67,42 @@ namespace PropHunt.EditMode.Tests.Mixed
             Entity player = CreateTestPlayer(new float3(0, 1, 0), 1.0f);
 
             this.buildPhysicsWorld.Update();
+            base.m_Manager.CompleteAllJobs();
             this.kccGroundedSystem.Update();
+
+            foreach (ComponentType type in base.m_Manager.GetComponentTypes(player))
+            {
+                UnityEngine.Debug.Log(type);
+            }
+
+            KCCGrounded grounded = base.m_Manager.GetComponentData<KCCGrounded>(player);
+            Assert.IsFalse(grounded.onGround);
+            Assert.IsTrue(grounded.distanceToGround == -1);
+            Assert.IsTrue(grounded.angle == -1);
+            Assert.IsTrue(grounded.groundedRBIndex == -1);
+            Assert.IsTrue(math.all(grounded.groundedPoint == float3.zero));
+            Assert.IsTrue(grounded.hitEntity == Entity.Null);
+            Assert.IsTrue(math.all(grounded.surfaceNormal == float3.zero));
         }
 
         [Test]
         public void TestGroundedWithFloor()
         {
-            Entity player = CreateTestPlayer(new float3(0, 1, 0), 1.0f);
+            Entity player = CreateTestPlayer(new float3(0, 0, 0), 1f);
+            base.m_Manager.SetComponentData(player, new Translation { Value = new float3(0, 1, 0) });
             Entity floor = PhysicsTestUtils.CreateBox(base.m_Manager, new float3(1, 1, 1), float3.zero, 0, quaternion.Euler(float3.zero), false);
 
+            UnityEngine.Debug.Log(base.m_Manager.CreateEntityQuery(typeof(KCCGrounded), typeof(KCCGravity), typeof(PhysicsCollider), typeof(Translation), typeof(Rotation)).CalculateEntityCount());
+
             this.buildPhysicsWorld.Update();
+            base.m_Manager.CompleteAllJobs();
             this.kccGroundedSystem.Update();
+
+            KCCGrounded grounded = base.m_Manager.GetComponentData<KCCGrounded>(player);
+            Assert.IsTrue(grounded.onGround);
+            Assert.IsTrue(grounded.distanceToGround >= 0);
+            Assert.IsTrue(grounded.angle != -1);
+            Assert.IsTrue(grounded.hitEntity == floor);
         }
     }
 }
