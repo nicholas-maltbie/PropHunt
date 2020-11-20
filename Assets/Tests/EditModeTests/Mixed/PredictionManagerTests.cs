60bcb530f1ce99dec4c0036f979f9dc59f722c22
Nick Maltbie <nick.dmalt@gmail.com>
2020-11-20 16:04:54 -0500

    Added tests for PredictionManager

---
+++ Assets/Tests/EditModeTests/Mixed/PredictionManagerTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using PropHunt.Game;
+using PropHunt.InputManagement;
+using PropHunt.Mixed.Utilities;
+using Unity.Entities.Tests;
+using Unity.NetCode;
+
+namespace PropHunt.EditMode.Tests.Mixed
+{
+    [TestFixture]
+    public class PredictionManagerTests : ECSTestsFixture
+    {
+        [Test]
+        public void VerifyPredictionManagerInvokeWithoutErrors()
+        {
+            IPredictionState predManager = new PredictionState();
+            base.World.CreateSystem<GhostPredictionSystemGroup>();
+            Assert.IsTrue(predManager.GetPredictingTick(base.World) >= 0u);
+        }
+    }
+}
